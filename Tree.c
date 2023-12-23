255a060fe7a6ce1b4171e3ac2d3cc4ad3ac79bbf
xsvmyx <125303992+xsvmyx@users.noreply.github.com>
2023-12-23 18:43:30 +0200

    Update Tree.c
    Working DrawTree Function
    Zoom and Moving Camera added

--- Tree.c
+++ Tree.c
@@ -1,4 +1,5 @@
 #include "Tree.h"
+#define MAX 4
 
 
 void Drawnode(int val,int posx,int posy){
@@ -6,21 +7,41 @@ void Drawnode(int val,int posx,int posy){
         DrawText(TextFormat("%d",val),posx-2,posy-6,15,WHITE);
 }
 
- void DrawTree(tree* a,int x,int y,int l,int depth){
+void DrawTree(tree* a,int x,int y,int l,int depth){
         if(a!=NULL){
-//         	printf("\n-----------\nthe node is val is : %d", a->val);
-        	Drawnode(a->val,x,y);
-/*        	printf("\nits children are:");
-        	for(int i =0; i < a->Nodes ;i++){
-        		if(a->Child[i] == NULL){
-        			break;
-				}
-				printf("%d,",a->Child[i]->val);
-			}*/
-       		for(int i =0; i < a->Nodes ;i++){
-        		DrawTree(a->Child[i], x+(i -1)* (l/pow(2,depth)) ,y+50,l/2,depth);
-			}
+           
+        Drawnode(a->val,x,y);
         
+         int k = pow(4,depth)-1;
+        
+        
+         if (a->child[0]!= NULL) {
+       
+         DrawTree( a->child[0] ,x-3*(l/k)/2,y+300,l,depth+1);
+         DrawLine( x-9 , y-9 , x-3*(l/k)/2-9 , y+300-9 , BLUE );
+         }
+        
+         if (a->child[1] != NULL) {
+        
+         DrawTree(a->child[1],x-(l/k)/2,y+300,l,depth+1);
+         DrawLine(x+9,y-9,x-(l/k)/2+9,y+300-9,BLUE);
+         }
+        
+         
+         if (a->child[2] != NULL) {
+        
+        DrawTree(a->child[2],x+(l/k)/2,y+300,l,depth+1);
+        DrawLine(x+9,y-9,x+(l/k)/2+9,y+300-9,BLUE);
+         }
+         
+         if (a->child[3] != NULL) {
+        
+        DrawTree(a->child[3],x+3*(l/k)/2,y+300,l,depth+1);
+        DrawLine(x+9,y-9,x+3*(l/k)/2+9,y+300-9,BLUE);
+         }
+        
+         
+         
         } 
  }
 /*
@@ -50,21 +71,22 @@ tree* newnode(int x, int Nodes){
     
     return t;
 }
-/*
-tree* newnode(int x, int Nodes){
+
+
+tree* newnode2(int x){
     tree* t;
-    t=MAKE(tree,1);//malloc(sizeof(tree));
-    t->Nodes = Nodes;
+    t=malloc(sizeof(tree));
     t->val=x;
-    t->left=NULL;
-    t->right=NULL;
-    for(int i = 0; i<t->Nodes;i++){
-    	
-	}
+    t->child=malloc(sizeof(tree)*MAX);
+    
+    for(int i=0;i<MAX;i++){
+    t->child[i]=NULL;
+    }
     
     return t;
 }
-*/
+
+
 void insert(tree** t,int x, int Nodes){
     if(*t==NULL){
         *t=newnode(x,Nodes);
@@ -85,3 +107,114 @@ void insert(tree** t,int x, int Nodes){
       
          
 } 
+
+
+
+
+
+
+
+int main() {
+ 
+tree* a;
+a=newnode2(1);
+
+a->child[0]=newnode2(2);
+a->child[1]=newnode2(3);
+a->child[2]=newnode2(4);
+a->child[3]=newnode2(5);
+
+a->child[0]->child[0]=newnode2(6);
+a->child[0]->child[1]=newnode2(7);
+a->child[0]->child[2]=newnode2(8);
+a->child[0]->child[3]=newnode2(9);
+
+a->child[1]->child[0]=newnode2(10);
+a->child[1]->child[1]=newnode2(11);
+a->child[1]->child[2]=newnode2(12);
+a->child[1]->child[3]=newnode2(13);
+
+a->child[0]->child[0]->child[0]=newnode2(6);
+a->child[0]->child[0]->child[1]=newnode2(6);
+a->child[0]->child[0]->child[2]=newnode2(6);
+a->child[0]->child[0]->child[3]=newnode2(6);
+
+a->child[0]->child[1]->child[0]=newnode2(6);
+a->child[0]->child[1]->child[1]=newnode2(6);
+a->child[0]->child[1]->child[2]=newnode2(6);
+a->child[0]->child[1]->child[3]=newnode2(6);
+
+a->child[0]->child[2]->child[0]=newnode2(6);
+a->child[0]->child[2]->child[1]=newnode2(6);
+a->child[0]->child[2]->child[2]=newnode2(6);
+a->child[0]->child[2]->child[3]=newnode2(6);
+
+a->child[0]->child[3]->child[0]=newnode2(6);
+a->child[0]->child[3]->child[1]=newnode2(6);
+a->child[0]->child[3]->child[2]=newnode2(6);
+a->child[0]->child[3]->child[3]=newnode2(6);
+
+
+
+
+//scanf l 
+int l=4;
+//l is the number of lines 
+
+ Camera2D cd;
+    (cd.offset).x=540;
+    cd.offset.y=350;
+    
+    cd.target.x=540;
+    cd.target.y=350;
+    cd.rotation=0;
+    cd.zoom=1;
+    
+    InitWindow(screenWidth, screenHeight, "Arbre n-aire ");
+
+
+
+    SetTargetFPS(60);
+
+
+    while (!WindowShouldClose()) {
+        
+        
+        
+        BeginDrawing();
+        BeginMode2D(cd);
+        
+        ClearBackground(BRITISH);
+
+
+        // l=5:  2^(5-2)= 8*screenWidth space needed
+        // l=4:  2^(4-2)= 4*screenWidth space needed
+        // l=3:  2^(3-2)= 2*screenWidth space needed
+        // etc...
+        
+        
+        
+        DrawTree(a,screenWidth/2,50,pow(2,l-2)*screenWidth,1);
+   
+        //camera commands
+         if(IsMouseButtonDown(0)) cd.zoom+=0.02;
+          if(IsMouseButtonDown(1)) cd.zoom-=0.02; 
+          if(IsKeyDown(262)) cd.target.x+=15;
+          if(IsKeyDown(263)) cd.target.x-=15;
+          if(IsKeyDown(264)) cd.target.y+=15;
+          if(IsKeyDown(265)) cd.target.y-=15;    
+    
+        EndMode2D();
+        EndDrawing();
+        
+        
+    }
+
+   
+   
+
+   
+    CloseWindow();
+
+    return 0;
+}
